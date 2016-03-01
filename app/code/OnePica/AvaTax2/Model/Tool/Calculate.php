a15990498157932d044f7cee23bb47d649344bfb
Rostyslav Tymoshenko <krifollk@gmail.com>
2016-03-01 16:39:44 +0200

    Implemented AV2-169: Implement Tool classes - Added Calculate tool

---
+++ app/code/OnePica/AvaTax2/Model/Tool/Calculate.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * OnePica_AvaTax2
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0),
+ * a copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+namespace OnePica\AvaTax2\Model\Tool;
+
+use Magento\Quote\Api\Data\ShippingAssignmentInterface;
+use OnePica\AvaTax2\Api\ResultInterface;
+use OnePica\AvaTax2\Helper\Config as ConfigHelper;
+use OnePica\AvaTax2\Model\ServiceFactory;
+
+/**
+ * Class Calculate
+ *
+ * @package OnePica\AvaTax2\Model\Tool
+ */
+class Calculate extends AbstractTool
+{
+    /**
+     * Shipping assignment
+     *
+     * @var \Magento\Quote\Api\Data\ShippingAssignmentInterface
+     */
+    protected $shippingAssignment;
+
+    /**
+     * Calculate constructor.
+     *
+     * @param \OnePica\AvaTax2\Helper\Config                      $config
+     * @param \OnePica\AvaTax2\Model\ServiceFactory               $serviceFactory
+     * @param \Magento\Quote\Api\Data\ShippingAssignmentInterface $shippingAssignment
+     */
+    public function __construct(
+        ConfigHelper $config,
+        ServiceFactory $serviceFactory,
+        ShippingAssignmentInterface $shippingAssignment
+    ) {
+        parent::__construct($config, $serviceFactory);
+        $this->shippingAssignment = $shippingAssignment;
+    }
+
+    /**
+     * Execute
+     *
+     * @return ResultInterface
+     */
+    public function execute()
+    {
+        return $this->getService()->calculate($this->shippingAssignment);
+    }
+}
