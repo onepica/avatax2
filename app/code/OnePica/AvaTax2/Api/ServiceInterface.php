2b4c182b5b4135ca6ad51ee5070162513a1a6ca5
Rostyslav Tymoshenko <krifollk@gmail.com>
2016-03-01 16:05:11 +0200

    Implemented AV2-169: Implement Tool classes - Added Service interface

---
+++ app/code/OnePica/AvaTax2/Api/ServiceInterface.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * OnePica_AvaTax2
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0),
+ * a copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+namespace OnePica\AvaTax2\Api;
+
+use Magento\Framework\DataObject;
+use Magento\Quote\Api\Data\ShippingAssignmentInterface;
+use Magento\Sales\Model\Order\Creditmemo;
+use Magento\Sales\Model\Order\Invoice;
+use Magento\Store\Model\Store;
+
+/**
+ * Interface ServiceInterface
+ *
+ * @package OnePica\AvaTax2\Api
+ */
+interface ServiceInterface
+{
+    /**
+     * Invoice
+     *
+     * @param \Magento\Sales\Model\Order\Invoice $invoice
+     * @return ResultInterface
+     */
+    public function invoice(Invoice $invoice);
+
+    /**
+     * Creditmemo
+     *
+     * @param \Magento\Sales\Model\Order\Creditmemo $creditmemo
+     * @return ResultInterface
+     */
+    public function creditmemo(Creditmemo $creditmemo);
+
+    /**
+     * Validate
+     *
+     * @param DataObject $object
+     * @todo need to specify which object ($object) will be passed to this method
+     * @return ResultInterface
+     */
+    public function validate($object);
+
+    /**
+     * Creditmemo
+     *
+     * @param \Magento\Quote\Api\Data\ShippingAssignmentInterface $shippingAssignment
+     * @return ResultInterface
+     */
+    public function calculate(ShippingAssignmentInterface $shippingAssignment);
+
+    /**
+     * Ping
+     *
+     * @param \Magento\Store\Model\Store $store
+     * @return ResultInterface
+     */
+    public function ping(Store $store);
+}
