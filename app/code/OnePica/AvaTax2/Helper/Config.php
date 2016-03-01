dbac4e3cd03c810129690f970aa639da7fea017a
Rostyslav Tymoshenko <krifollk@gmail.com>
2016-03-01 17:03:40 +0200

    Implemented AV2-169: Implement Tool classes - Added config helper with getActiveService method - Added config.xml

---
+++ app/code/OnePica/AvaTax2/Helper/Config.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * OnePica_AvaTax2
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0),
+ * a copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+namespace OnePica\AvaTax2\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+
+/**
+ * Class Config
+ *
+ * @package OnePica\AvaTax2\Helper
+ */
+class Config extends AbstractHelper
+{
+    /**
+     * Xml path to active service
+     */
+    const AVATAX_ACTIVE_SERVICE = 'tax/avatax/service_type';
+
+    /**
+     * Get active service
+     *
+     * @return string
+     */
+    public function getActiveService()
+    {
+        return $this->scopeConfig->getValue(self::AVATAX_ACTIVE_SERVICE);
+    }
+}
