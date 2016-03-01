9e7153637be193c07bbb77ea674f4ecb1647a216
Rostyslav Tymoshenko <krifollk@gmail.com>
2016-03-01 16:06:17 +0200

    Implemented AV2-169: Implement Tool classes - Added Tool interface

---
+++ app/code/OnePica/AvaTax2/Api/ToolInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * OnePica_AvaTax2
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0),
+ * a copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+namespace OnePica\AvaTax2\Api;
+
+/**
+ * Interface ToolInterface
+ *
+ * @package OnePica\AvaTax2\Api
+ */
+interface ToolInterface
+{
+    /**
+     * Execute
+     *
+     * @return ResultInterface
+     */
+    public function execute();
+}
