64252b337adabb9e161a8473d94d92fdfdc08e27
Rostyslav Tymoshenko <krifollk@gmail.com>
2016-03-01 16:05:48 +0200

    Implemented AV2-169: Implement Tool classes - Added Service factory

---
+++ app/code/OnePica/AvaTax2/Model/ServiceFactory.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * OnePica_AvaTax2
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0),
+ * a copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+namespace OnePica\AvaTax2\Model;
+
+use Magento\Framework\ObjectManager\ObjectManager;
+use OnePica\AvaTax2\Api\ServiceInterface;
+
+/**
+ * Class ServiceFactory
+ *
+ * @package OnePica\AvaTax2\Model
+ */
+class ServiceFactory
+{
+    /**
+     * Object manager
+     *
+     * @var \Magento\Framework\ObjectManager\ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * ServiceFactory constructor.
+     *
+     * @param \Magento\Framework\ObjectManager\ObjectManager $objectManager
+     */
+    public function __construct(ObjectManager $objectManager)
+    {
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * Create service
+     *
+     * @param string $service
+     * @return ServiceInterface
+     */
+    public function create($service)
+    {
+        return $this->objectManager->get($service);
+    }
+}
