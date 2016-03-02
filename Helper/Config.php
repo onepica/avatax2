44e005849940a3fa9702c2168bc51ab90fb272d3
Rostyslav Tymoshenko <krifollk@gmail.com>
2016-03-02 14:06:49 +0200

    Implemented AV2-172: Implement Connection Settings section in admin config - Added methods to retrieving connection settings value

--- Helper/Config.php
+++ Helper/Config.php
@@ -15,6 +15,8 @@
 namespace OnePica\AvaTax\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\Store;
 
 /**
  * Class Config
@@ -23,10 +25,16 @@ use Magento\Framework\App\Helper\AbstractHelper;
  */
 class Config extends AbstractHelper
 {
-    /**
-     * Xml path to active service
+    /**#@+
+     * Config xml path
      */
-    const AVATAX_ACTIVE_SERVICE = 'tax/avatax/active_service';
+    const AVATAX_ACTIVE_SERVICE         = 'tax/avatax/active_service';
+    const AVATAX_SERVICE_ACTION         = 'tax/avatax/action';
+    const AVATAX_SERVICE_URL            = 'tax/avatax/url';
+    const AVATAX_SERVICE_ACCOUNT_NUMBER = 'tax/avatax/account_number';
+    const AVATAX_SERVICE_LICENCE_KEY    = 'tax/avatax/license_key';
+    const AVATAX_SERVICE_COMPANY_CODE   = 'tax/avatax/company_code';
+    /**#@-*/
 
     /**
      * Get active service
@@ -36,5 +44,73 @@ class Config extends AbstractHelper
     public function getActiveService()
     {
         return $this->scopeConfig->getValue(self::AVATAX_ACTIVE_SERVICE);
+    }
+
+    /**
+     * Get service action
+     *
+     * @param Store|int $store
+     * @return int
+     */
+    public function getServiceAction($store = null)
+    {
+        return (int)$this->getConfig(self::AVATAX_SERVICE_ACTION, $store);
+    }
+
+    /**
+     * Get service url
+     *
+     * @param Store|int $store
+     * @return string
+     */
+    public function getServiceUrl($store = null)
+    {
+        return (string)$this->getConfig(self::AVATAX_SERVICE_URL, $store);
+    }
+
+    /**
+     * Get service account number
+     *
+     * @param Store|int $store
+     * @return string
+     */
+    public function getServiceAccountNumber($store = null)
+    {
+        return (string)$this->getConfig(self::AVATAX_SERVICE_ACCOUNT_NUMBER, $store);
+    }
+
+    /**
+     * Get service licence key
+     *
+     * @param Store|int $store
+     * @return string
+     */
+    public function getServiceLicenceKey($store = null)
+    {
+        return (string)$this->getConfig(self::AVATAX_SERVICE_LICENCE_KEY, $store);
+    }
+
+    /**
+     * Get service company code
+     *
+     * @param Store|int $store
+     * @return string
+     */
+    public function getServiceCompanyCode($store = null)
+    {
+        return (string)$this->getConfig(self::AVATAX_SERVICE_COMPANY_CODE, $store);
+    }
+
+    /**
+     * Get config
+     *
+     * @param string    $path
+     * @param Store|int $store
+     * @param string    $scopeType
+     * @return mixed
+     */
+    protected function getConfig($path, $store = null, $scopeType = ScopeInterface::SCOPE_STORE)
+    {
+        return $this->scopeConfig->getValue($path, $scopeType, $store);
     }
 }
