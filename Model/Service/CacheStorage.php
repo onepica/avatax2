bf3bee55c8a4da79f5259a283522ddd43291fe0c
Anatolii Khoma <a.khoma@astoundcommerce.com>
2016-03-04 18:51:50 +0200

    Implemented AV2-178: Implement address validation service -CacheStorage class

---
+++ Model/Service/CacheStorage.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * OnePica_AvaTax
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0),
+ * a copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+namespace OnePica\AvaTax\Model\Service;
+
+use Magento\Customer\Model\Session;
+
+/**
+ * Class CacheStorage
+ *
+ * @package OnePica\AvaTax\Model\Service
+ */
+
+class CacheStorage
+{
+    /**
+     * Id of cache storage
+     *
+     * @var string
+     */
+    protected $cacheId;
+
+    /**
+     * Data
+     *
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * Data
+     *
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * DataCache constructor
+     *
+     * @param Session $session
+     */
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * Get cache Id
+     *
+     * @return string
+     */
+    public function getCacheId()
+    {
+        return $this->cacheId;
+    }
+
+    /**
+     * Set cache Id
+     *
+     * @param string $value
+     * @return $this
+     */
+    public function setCacheId($value)
+    {
+        $this->cacheId = $value;
+        $this->loadDataFromSession();
+
+        return $this;
+    }
+
+    /**
+     * Load data from session
+     *
+     * @return $this
+     */
+    protected function loadDataFromSession()
+    {
+        $data = $this->session->getData($this->cacheId);
+        if ($data) {
+            $this->data = unserialize($data);
+        }
+    }
+
+    /**
+     * Load data from session
+     *
+     * @return $this
+     */
+    protected function saveDataToSession()
+    {
+        $sessionMethodName = 'set' . ucfirst($this->cacheId);
+        $this->session->$sessionMethodName(serialize($this->data));
+
+        return $this;
+    }
+
+    /**
+     * Get data item by key
+     *
+     * @param string $key
+     * @return null|mixed
+     */
+    public function get($key)
+    {
+        return isset($this->data[$key]) ? $this->data[$key] : null;
+    }
+
+    /**
+     * Put data item to cache storage
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return $this
+     */
+    public function put($key, $value)
+    {
+        $this->data[$key] = $value;
+        $this->saveDataToSession();
+
+        return $this;
+    }
+}
