d3c59190bbeb6840387e2d07ef22950f20ebcd27
Anatolii Khoma <a.khoma@astoundcommerce.com>
2016-03-23 16:17:01 +0200

    AV2-231:Create grid

---
+++ Controller/Adminhtml/Queue/index.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * OnePica_AvaTax
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0),
+ * a copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+namespace OnePica\AvaTax\Controller\Adminhtml\Queue;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\View\Result\PageFactory;
+use OnePica\AvaTax\Controller\Adminhtml\AbstractQueueAction;
+
+/**
+ * Class Index
+ *
+ * @package OnePica\AvaTax\Controller\Adminhtml\Log
+ */
+class Index extends AbstractQueueAction
+{
+    /**
+     * Result page factory
+     *
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     *
+     * @param \Magento\Backend\App\Action\Context        $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(Context $context, PageFactory $resultPageFactory)
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Dispatch request
+     *
+     * @return \Magento\Framework\Controller\ResultInterface|ResponseInterface
+     * @throws \Magento\Framework\Exception\NotFoundException
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('OnePica_AvaTax::sales_avatax_queue');
+        $resultPage->getConfig()->getTitle()->prepend(__('AvaTax Order Sync Queue'));
+
+        return $resultPage;
+    }
+}
