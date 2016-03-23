7c7de10707f4954818a8f7729401c09bd7df0801
Anatolii Khoma <a.khoma@astoundcommerce.com>
2016-03-23 18:46:14 +0200

    AV2-232:Create buttons with actions

---
+++ Controller/Adminhtml/Queue/ProcessQueue.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * OnePica_AvaTax
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0),
+ * a copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+namespace OnePica\AvaTax\Controller\Adminhtml\Queue;
+
+use OnePica\AvaTax\Controller\Adminhtml\AbstractQueueAction;
+
+/**
+ * Class ProcessQueue
+ *
+ * @package OnePica\AvaTax\Controller\Adminhtml\Log
+ */
+class ProcessQueue extends AbstractQueueAction
+{
+    /**
+     * Dispatch request
+     */
+    public function execute()
+    {
+        exit('Process');
+    }
+}
