227967629fabf7da66c6d7cc0ce5b8287d37971a
Rostyslav Tymoshenko <krifollk@gmail.com>
2016-04-07 15:42:11 +0300

    Implemented AV2-207: Implement possibility to save and display Fixed Product Tax (FPT) - Hide some default FPT settings

---
+++ Plugin/Magento/Weee/Model/Config.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * OnePica_AvaTax
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0),
+ * a copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+namespace OnePica\AvaTax\Plugin\Magento\Weee\Model;
+
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Weee\Model\Tax as WeeeModelTax;
+
+/**
+ * Class Config
+ *
+ * @package OnePica\AvaTax\Plugin\Magento\Weee\Model
+ */
+class Config
+{
+    /**
+     * Avatax config helper
+     *
+     * @var \OnePica\AvaTax\Helper\Config
+     */
+    protected $config;
+
+    /**
+     * Store manager
+     * 
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * Config constructor.
+     *
+     * @param \OnePica\AvaTax\Helper\Config $config
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(\OnePica\AvaTax\Helper\Config $config, StoreManagerInterface $storeManager)
+    {
+        $this->config = $config;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * After isEnable method plugin
+     *
+     * @param \Magento\Weee\Model\Config $config
+     * @param $result
+     * @return bool
+     */
+    public function afterIsEnabled(\Magento\Weee\Model\Config $config, $result)
+    {
+        if ($this->config->isAvaTaxEnabled($this->storeManager->getStore())) {
+            return true;
+        }
+
+        return $result;
+    }
+
+    /**
+     * After isTaxable method plugin
+     *
+     * @param \Magento\Weee\Model\Config $config
+     * @param $result
+     * @return bool
+     */
+    public function afterIsTaxable(\Magento\Weee\Model\Config $config, $result)
+    {
+        if ($this->config->isAvaTaxEnabled($this->storeManager->getStore())) {
+            return false;
+        }
+
+        return $result;
+    }
+
+    /**
+     * After getListPriceDisplayType method plugin
+     *
+     * @param \Magento\Weee\Model\Config $config
+     * @param int $result
+     * @return int
+     */
+    public function afterGetListPriceDisplayType(\Magento\Weee\Model\Config $config, $result)
+    {
+        if ($this->config->isAvaTaxEnabled($this->storeManager->getStore())) {
+            return WeeeModelTax::DISPLAY_EXCL;
+        }
+
+        return $result;
+    }
+
+    /**
+     * After getPriceDisplayType method plugin
+     *
+     * @param \Magento\Weee\Model\Config $config
+     * @param int $result
+     * @return int
+     */
+    public function afterGetPriceDisplayType(\Magento\Weee\Model\Config $config, $result)
+    {
+        if ($this->config->isAvaTaxEnabled($this->storeManager->getStore())) {
+            return WeeeModelTax::DISPLAY_EXCL;
+        }
+
+        return $result;
+    }
+}
