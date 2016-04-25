e9c5f39afacbc64ac9dc53bf34e7573bb022eb7e
Anatolii Khoma <a.khoma@astoundcommerce.com>
2016-04-25 11:58:31 +0300

    Fixed AV2-1107: The Notification Toolbar field under Error Handling block is missing -PHPDoc fix

--- Model/AdminNotification/System/Message/QueueError.php
+++ Model/AdminNotification/System/Message/QueueError.php
@@ -1,7 +1,16 @@
 <?php
 /**
- * Copyright © 2015 Magento. All rights reserved.
- * See COPYING.txt for license details.
+ * OnePica_AvaTax
+ * NOTICE OF LICENSE
+ * This source file is subject to the Open Software License (OSL 3.0),
+ * a copy of which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   OnePica
+ * @package    OnePica_AvaTax
+ * @author     OnePica Codemaster <codemaster@onepica.com>
+ * @copyright  Copyright (c) 2016 One Pica, Inc.
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  */
 
 namespace OnePica\AvaTax\Model\AdminNotification\System\Message;
