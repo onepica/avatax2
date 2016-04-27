e383f14c98ef5b31d326d460b7b6dca3c27a328e
Rostyslav Tymoshenko <krifollk@gmail.com>
2016-04-27 16:54:28 +0300

    Fixed AV2-1138: "You are using the AvaTax development connection URL" and "Orders will not be sent to the AvaTax system" messages are missing - Added necessary notifications

--- Observer/ChangedSectionTax.php
+++ Observer/ChangedSectionTax.php
@@ -19,6 +19,7 @@ use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Message\ManagerInterface;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Store\Model\StoreManagerInterface;
+use OnePica\AvaTax\Helper\Config;
 use OnePica\AvaTax\Model\Tool\Ping;
 
 /**
@@ -50,38 +51,70 @@ class ChangedSectionTax implements ObserverInterface
     protected $manager;
 
     /**
+     * Config
+     *
+     * @var Config
+     */
+    protected $config;
+
+    /**
      * ChangedSectionTax constructor.
      *
      * @param StoreManagerInterface                     $storeManager
      * @param \Magento\Framework\ObjectManagerInterface $objectManager
      * @param ManagerInterface                          $manager
+     * @param Config                                    $config
      */
     public function __construct(
         StoreManagerInterface $storeManager,
         ObjectManagerInterface $objectManager,
-        ManagerInterface $manager
+        ManagerInterface $manager,
+        Config $config
     ) {
         $this->storeManager = $storeManager;
         $this->objectManager = $objectManager;
         $this->manager = $manager;
+        $this->config = $config;
     }
 
     /**
      * Execute
      *
      * @param Observer $observer
+     *
      * @return void
      */
     public function execute(Observer $observer)
     {
         $store = $this->storeManager->getStore((int)$observer->getData('store'));
-        $this->sendPing($store);
+        $this->sendPing($store)->showNotifications($store);
+    }
+
+    /**
+     * Show notification
+     *
+     * @param \Magento\Store\Api\Data\StoreInterface $store
+     *
+     * @return $this
+     */
+    protected function showNotifications($store)
+    {
+        if ($this->config->getServiceUrl($store) === \OnePica\AvaTax\Model\Source\Avatax16\Url::DEVELOPER_URL) {
+            $this->manager->addNotice(__('You are using the AvaTax development connection URL.'));
+        }
+
+        if ($this->config->getServiceAction($store) === \OnePica\AvaTax\Model\Source\Avatax16\Action::ACTION_CALC) {
+            $this->manager->addNotice(__('Orders will not be sent to the AvaTax system.'));
+        }
+        
+        return $this;
     }
 
     /**
      * Send ping
      *
      * @param \Magento\Store\Api\Data\StoreInterface $store
+     *
      * @return \OnePica\AvaTax\Model\Service\Result\Base
      */
     protected function sendPing($store)
