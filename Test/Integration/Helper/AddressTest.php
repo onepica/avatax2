8e8d179ae6503e5d907f33c20b0b36d4324acb39
Anatolii Khoma <a.khoma@astoundcommerce.com>
2016-06-29 18:09:39 +0300

    Implemented AV2-1173: Implement integration test for Address helper -isObjectActionable method

--- Test/Integration/Helper/AddressTest.php
+++ Test/Integration/Helper/AddressTest.php
@@ -83,4 +83,42 @@ class AddressTest extends \PHPUnit_Framework_TestCase
         $filterMode = 1;
         $this->assertFalse($this->addressHelper->isAddressActionable($address, $store, $filterMode, true));
     }
+
+    /**
+     * @magentoDataFixture   ../../../../app/code/Astound/AvaTax/Test/Integration/Helper/_files/invoice.php
+     * @magentoConfigFixture current_store tax/avatax/general_group/action 2
+     * @magentoConfigFixture current_store tax/avatax/region_filter_group/taxable_country CA,US
+     * @magentoConfigFixture current_store tax/avatax/region_filter_group/region_filter_mode 2
+     * @magentoConfigFixture current_store tax/avatax/region_filter_group/region_filter_list 12,43
+     *
+     * @magentoConfigFixture current_store tax/avatax/general_group/url test
+     * @magentoConfigFixture current_store tax/avatax/general_group/account_number test
+     * @magentoConfigFixture current_store tax/avatax/general_group/license_key test
+     * @magentoConfigFixture current_store tax/avatax/general_group/company_code test
+     *
+     * @magentoAppIsolation enabled
+     * @magentoDbIsolation  enabled
+     */
+    public function testIsObjectActionable()
+    {
+        /** @var \Magento\Framework\Registry $registry */
+        $registry = $this->objectManager->get('Magento\Framework\Registry');
+        $invoice = $registry->registry('astound_avatax_invoice');
+        $address = $invoice->getShippingAddress();
+        $defaultAddressData = $address->getData();
+
+        // actionable case
+        $this->assertTrue($this->addressHelper->isObjectActionable($invoice));
+
+        // not actionable case
+        // not taxable country
+        $address->setCountryId('SK');
+        $this->assertFalse($this->addressHelper->isObjectActionable($invoice));
+
+        // not actionable case
+        // not actionable region
+        $address->setData($defaultAddressData);
+        $address->setRegionId(1);
+        $this->assertFalse($this->addressHelper->isObjectActionable($invoice));
+    }
 }
--- Test/Integration/Helper/_files/customer_address.php
+++ Test/Integration/Helper/_files/customer_address.php
@@ -2,26 +2,16 @@
 
 use Magento\TestFramework\Helper\Bootstrap;
 
+$objectManager = Bootstrap::getObjectManager();
+
 /** @var \Magento\Customer\Model\Address $customerAddress */
-$customerAddress = Bootstrap::getObjectManager()->create('Magento\Customer\Model\Address');
+$customerAddress = $objectManager->create('Magento\Customer\Model\Address');
 $customerAddress->isObjectNew(true);
-$customerAddress->setCustomerId(1)->setData(
-    [
-        'entity_id'        => 1,
-        'attribute_set_id' => 2,
-        'telephone'        => 123456789,
-        'postcode'         => 10038,
-        'country_id'       => 'US',
-        'city'             => 'NewYork',
-        'company'          => 'CompanyName',
-        'street'           => '350th 5 Ave',
-        'lastname'         => 'Smith',
-        'firstname'        => 'John',
-        'parent_id'        => 1,
-        'region_id'        => 43,
-    ]
-);
+
+$addressData = include __DIR__ . '/address_data.php';
+
+$customerAddress->setCustomerId(1)->setData($addressData);
 $customerAddress->save();
 
-$objectManager = Bootstrap::getObjectManager();
+
 $objectManager->get('Magento\Framework\Registry')->register('astound_avatax_customer_address', $customerAddress);
